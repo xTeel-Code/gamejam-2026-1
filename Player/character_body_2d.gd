5d3c242b7228bffc936128723be0da8b6bc09715
adamzlatos <adamzlatos.az@gmail.com>
2026-02-05 21:36:05 +0100

    konecne skace ten  curak

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -3,14 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
-
+var jump_counter = 0
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_counter = 0
+	if Input.is_action_just_pressed("ui_accept") and jump_counter == 0 and is_on_floor() == false: 
+		velocity.y = JUMP_VELOCITY
+		jump_counter += 1
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
 		$Sprite2D.flip_h =true
