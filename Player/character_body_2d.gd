6757475ad0cced4d57b066adfc7ff94675f21977
xTeel-Code <samuel.bencc@gmail.com>
2026-02-05 23:34:00 +0100

    Last Resort Jump lebo tato hra je vraj nehratelna bez neho (bullshit)

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -450.0
 var jump_counter = 0
-
+var last_resort_jump = 0
 func _physics_process(delta: float) -> void:
 
 	# Add the gravity.
@@ -16,6 +16,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and jump_counter == 0 and is_on_floor() == false: 
 		velocity.y = JUMP_VELOCITY
 		jump_counter += 1
+		last_resort_jump = 0
+	if !is_on_floor() and velocity.y > -300 and last_resort_jump<1:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			last_resort_jump += 1
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
 		$Sprite2D.flip_h =true
