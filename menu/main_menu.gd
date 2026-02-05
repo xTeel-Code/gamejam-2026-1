1b4220807700b297a1750e40b4b5efcaa6ba5f4a
samisami2209 <samuel.zalezak@gmail.com>
2026-02-05 22:52:49 +0100

    aadsafaswf

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,15 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	buttons.visible = true 
 	options.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 @onready var buttons: VBoxContainer = $Buttons
 @onready var options: Panel = $Options
