3f8cf28af3e67d284447e7300232cbac7c195b1d
xTeel-Code <samuel.bencc@gmail.com>
2026-02-06 00:01:20 +0100

    Uz pushujem jak Gunna a Future (Pushin P referencia)

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -2,25 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -460.0
 var jump_counter = 0
-var last_resort_jump = 0
 func _physics_process(delta: float) -> void:
-
-	# Add the gravity.
+	if is_on_floor():
+		jump_counter = 0
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if Input.is_action_just_pressed("ui_accept") and jump_counter < 0:
+			velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		jump_counter = 0
 	if Input.is_action_just_pressed("ui_accept") and jump_counter == 0 and is_on_floor() == false: 
 		velocity.y = JUMP_VELOCITY
 		jump_counter += 1
-		last_resort_jump = 0
-	if !is_on_floor() and velocity.y > -300 and last_resort_jump<1:
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
-			last_resort_jump += 1
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
 		$Sprite2D.flip_h =true
