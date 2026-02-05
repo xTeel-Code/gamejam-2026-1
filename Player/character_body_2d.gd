8068c90e441cf46f880c96ef2c4e639de8e26db9
xTeel-Code <samuel.bencc@gmail.com>
2026-02-05 19:54:18 +0100

    Movementgit add .!

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -11,8 +11,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	var rnd = RandomNumberGenerator.new()
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		$Sprite2D.flip_h =true
+	elif direction < 0:
+		$Sprite2D.flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
 	else:
