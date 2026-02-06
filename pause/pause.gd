fef7c2e688568e737d081cdfac26166d71af7f78
samisami2209 <samuel.zalezak@gmail.com>
2026-02-06 02:00:33 +0100

    asv

--- pause/pause.gd
+++ pause/pause.gd
@@ -1,39 +1,50 @@
 extends Control
 
+@onready var buttons: VBoxContainer = $PanelContainer/Pauza
+@onready var options: Panel = $PanelContainer/Options
 
 func _ready():
 	hide()
-	
-func resume():
-	get_tree().paused = false
-	hide()
+	buttons.visible = true
+	options.visible = false
+	buttons.get_child(0).grab_focus()
 
 func pause():
 	get_tree().paused = true
 	show()
+	buttons.visible = true
+	options.visible = false
+	buttons.get_child(0).grab_focus()
 
+func resume():
+	get_tree().paused = false
+	hide()
 
-func testEsc():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
-		resume()
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		if options.visible:
+			_on_back_pressed()
+		elif get_tree().paused:
+			resume()
+		else:
+			pause()
 
 func _on_resume_pressed() -> void:
 	resume()
 
-
 func _on_options_pressed() -> void:
-	pass
+	buttons.visible = false
+	options.visible = true
+	options.get_child(0).grab_focus()
 
+func _on_back_pressed() -> void:
+	options.visible = false
+	buttons.visible = true
+	buttons.get_child(0).grab_focus()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_restart_pressed() -> void:
 	resume()
 	get_tree().reload_current_scene()
-
-func _process(delta):
-	testEsc()
