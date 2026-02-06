70acaa0bb99b18295d39836ccdd14822f51b3686
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-02-06 01:46:59 +0100

    zbytocny commit

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-var Health = 1
+var health = 1
 var has_key = false
 const SPEED = 400.0
 const JUMP_VELOCITY = -460.0
@@ -9,13 +9,14 @@ var height_before_land = 0
 var fall = 0
 
 func _physics_process(delta: float) -> void:
+	
 	if is_on_floor():
 		jump_counter = 0
 		height_before_land = position.y
 		fall = heigt_before_jump - height_before_land
 		heigt_before_jump = position.y
 		if fall < -500:
-			Health -= 1
+			health -= 1
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if Input.is_action_just_pressed("ui_accept") and jump_counter < 0:
