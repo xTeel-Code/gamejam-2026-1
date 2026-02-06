c399e61744b02ea5e74dc49fb4b6b696f1890f38
samisami2209 <samuel.zalezak@gmail.com>
2026-02-06 05:34:52 +0100

    NEVIEM

--- Level/area_2d.gd
+++ Level/area_2d.gd
@@ -1,7 +1,9 @@
 extends Area2D
+@onready var key: AudioStreamPlayer = $key
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		key.play()
 		reparent(body)
 		position = Vector2(80, -20)
 		body.has_key = true 
