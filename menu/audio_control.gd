2eb4b3a437cd569281447614c8a97a0aa4525467
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-02-06 14:10:35 +0100

    Finally gule

--- menu/audio_control.gd
+++ menu/audio_control.gd
@@ -7,7 +7,7 @@ var audio_bus_id
 func _ready():
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
 	
-func _on_value_changed(value: float) -> void:
+func _on_value_changed(_value: float) -> void:
 	drop.play()
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(audio_bus_id, db)
