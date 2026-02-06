e8bfdfe333021cba4ebce0769f32672fe82e6292
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-02-06 01:42:36 +0100

    Fall Damage Babyyyyy!

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
-
+var Health = 1
 var has_key = false
 const SPEED = 400.0
 const JUMP_VELOCITY = -460.0
 var jump_counter = 0
+var heigt_before_jump = 0
+var height_before_land = 0
+var fall = 0
+
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jump_counter = 0
+		height_before_land = position.y
+		fall = heigt_before_jump - height_before_land
+		heigt_before_jump = position.y
+		if fall < -500:
+			Health -= 1
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if Input.is_action_just_pressed("ui_accept") and jump_counter < 0:
@@ -16,7 +25,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and jump_counter == 0 and is_on_floor() == false: 
 		velocity.y = JUMP_VELOCITY
 		jump_counter += 1
-
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction > 0:
 		$Sprite2D.flip_h =true
