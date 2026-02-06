fec8da061758cfa454aed3e8f75e8ab880d70d61
samisami2209 <samuel.zalezak@gmail.com>
2026-02-06 05:15:57 +0100

    ZVUKY VIAC

--- Level/area_2d.gd
+++ Level/area_2d.gd
@@ -1,7 +1,9 @@
 extends Area2D
+@onready var key: AudioStreamPlayer = $key
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		key.play()
 		set_deferred("monitoring", false)
 		body.has_key = true
 		reparent(body)
--- Level/healthbar_2.gd
+++ Level/healthbar_2.gd
@@ -1,11 +1,13 @@
 extends AnimatedSprite2D
 var i = 0
 @onready var death: AudioStreamPlayer = $death
+@onready var wines: AudioStreamPlayer = $wines
 func Damage(StartPosition):
 	if i == 12:
 		pass
 	i+=2
 	death.play()
+	wines.play()
 	set_frame(i)
 	$"../Player".global_position = StartPosition
 	
