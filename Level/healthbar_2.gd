2ed8926298e0827ae991d40fe7e7a548aedf6dfa
adamzlatos <adamzlatos.az@gmail.com>
2026-02-06 15:00:27 +0100

    gds

--- Level/healthbar_2.gd
+++ Level/healthbar_2.gd
@@ -8,19 +8,19 @@ var deaths = 2
 func FallDamage(StartPosition):
 	if Globals.is_falldmg_on == true:
 		deaths += 2
-	if Globals.player_dmg_taken < 2:
+	if Globals.player_dmg_taken < deaths:
 		$"../Player".global_position = StartPosition 
 		Globals.get_hit()
 		var main_level = get_tree().current_scene 
 		key.reparent(main_level)
-		death.play()
-		wines.play()
+		#death.play()
+		#wines.play()
 		$"../Key".global_position = keyPosition
 		$"../Player".has_key = false
-		$"../Key".global_position = global_position 
+		$"../Key".global_position = global_position
 		key.reparent(main_level)
-		death.play()
-		wines.play()
+		#death.play()
+		#wines.play()
 		if Globals.player_dmg_taken == 2:
 			get_tree().change_scene_to_file("res://Level/Level_2.tscn")
 		if Globals.player_dmg_taken == 4:
