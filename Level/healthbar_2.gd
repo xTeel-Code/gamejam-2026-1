f9869a204e875362bb46f938cf3291488f1f2c6a
adamzlatos <adamzlatos.az@gmail.com>
2026-02-06 09:49:08 +0100

    cmmi

--- Level/healthbar_2.gd
+++ Level/healthbar_2.gd
@@ -2,20 +2,26 @@ extends AnimatedSprite2D
 var KeyPosition : Vector2
 @onready var keyPosition = $"../Key".global_position
 @onready var key = $"../Key"
-var i = 0
+var i = 2
 @onready var death: AudioStreamPlayer = $death
 @onready var wines: AudioStreamPlayer = $wines
 func Damage(StartPosition):
 	var main_level = get_tree().current_scene 
 	key.reparent(main_level)
 	$"../Key".global_position = global_position 
-	if i == 12:
+	if i == 2:
+		i+=2
+		print("aaaa")
+		death.play()
+		wines.play()
+		set_frame(i)
+		$"../Key".global_position = keyPosition
+		$"../Player".global_position = StartPosition
+		$"../Player".has_key = false
+		get_tree().change_scene_to_file("res://Level/Level_2.tscn")
+	elif i == 4:
+		get_tree().change_scene_to_file("res://Level/Level_3.tscn")
+	elif i == 12:
 		get_tree().change_scene_to_file("res://death_screen.tscn")
-	i+=2
-	death.play()
-	wines.play()
-	set_frame(i)
-	$"../Key".global_position = keyPosition
-	$"../Player".global_position = StartPosition
-	$"../Player".has_key = false
+	
 	
--- Level/mec_hitbox.gd
+++ Level/mec_hitbox.gd
@@ -2,9 +2,8 @@ extends Area2D
 
 @onready var healthbar_2: AnimatedSprite2D = %Healthbar2
 
-func _on_body_entered(body):
+
+func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		healthbar_2.Damage(body.start_position) 
 		print("ccs")
-		
-		
