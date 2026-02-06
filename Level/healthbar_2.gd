15b66bc54818b469e6792564cf5e55e5be07a8ec
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-02-06 16:11:32 +0100

    Finally Finally??

--- Level/healthbar_2.gd
+++ Level/healthbar_2.gd
@@ -2,8 +2,6 @@ extends AnimatedSprite2D
 var KeyPosition : Vector2
 @onready var keyPosition = $"../Key".global_position
 @onready var key = $"../Key"
-@onready var death: AudioStreamPlayer = $death
-@onready var wines: AudioStreamPlayer = $wines
 var deaths = 2
 func FallDamage(StartPosition):
 	if Globals.is_falldmg_on == true:
@@ -13,14 +11,10 @@ func FallDamage(StartPosition):
 		Globals.get_hit()
 		var main_level = get_tree().current_scene 
 		key.reparent(main_level)
-		#death.play()
-		#wines.play()
 		$"../Key".global_position = keyPosition
 		$"../Player".has_key = false
 		$"../Key".global_position = global_position
 		key.reparent(main_level)
-		#death.play()
-		#wines.play()
 		if Globals.player_dmg_taken == 2:
 			get_tree().change_scene_to_file("res://Level/Level_2.tscn")
 		if Globals.player_dmg_taken == 4:
