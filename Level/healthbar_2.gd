ad946f0eeaa719b0542252c88e1cc7c83ea28bae
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-02-06 11:27:10 +0100

    Update healthbar_2.gd

--- Level/healthbar_2.gd
+++ Level/healthbar_2.gd
@@ -5,15 +5,22 @@ var KeyPosition : Vector2
 @onready var death: AudioStreamPlayer = $death
 @onready var wines: AudioStreamPlayer = $wines
 func Damage(StartPosition):
-	Globals.get_hit()
-	var main_level = get_tree().current_scene 
-	key.reparent(main_level)
-	death.play()
-	wines.play()
-	$"../Key".global_position = keyPosition
-	$"../Player".global_position = StartPosition
-	$"../Player".has_key = false
-	$"../Key".global_position = global_position 
+	print(Globals.player_dmg_taken)
+	if Globals.player_dmg_taken < 4:
+		Globals.get_hit()
+		var main_level = get_tree().current_scene 
+		key.reparent(main_level)
+		death.play()
+		wines.play()
+		$"../Key".global_position = keyPosition
+		$"../Player".global_position = StartPosition
+		$"../Player".has_key = false
+		$"../Key".global_position = global_position 
+	elif Globals.player_dmg_taken >= 4:
+		var main_level = get_tree().current_scene 
+		key.reparent(main_level)
+		death.play()
+		wines.play()
 	if Globals.player_dmg_taken == 2:
 		get_tree().change_scene_to_file("res://Level/Level_2.tscn")
 	if Globals.player_dmg_taken == 4:
@@ -26,5 +33,3 @@ func Damage(StartPosition):
 		get_tree().change_scene_to_file("res://Level/Level_6.tscn")
 	elif Globals.player_dmg_taken == 12:
 		get_tree().change_scene_to_file("res://death_screen.tscn")
-	
-	
