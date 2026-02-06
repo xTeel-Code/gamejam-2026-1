ddc3675500a6fec78e775823e42a077682d93710
samisami2209 <samuel.zalezak@gmail.com>
2026-02-06 14:43:22 +0100

    pomoc

--- Level/level.gd
+++ Level/level.gd
@@ -1,6 +1,5 @@
 extends Node2D
 @onready var death: AudioStreamPlayer = $death
-@onready var fullscreen: CheckButton = $"PanelContainer/Options/Audio Control/Label2/Fullscreen"
 @onready var wines: AudioStreamPlayer = $wines
 func _ready() -> void:
 	death.play()
--- Level/level_1.gd
+++ Level/level_1.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
